bb9c1c96a359681382ab0afb51fe99018f86f3cc
pjulvez <pjulvez@almata.cat>
2020-11-23 17:21:41 +0100

    afegint els navigators

---
+++ js/navegador.js
@@ -0,0 +1,16 @@
+
+function informacio() {
+
+  var es_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+  if(es_chrome){
+            alert("El navegador que se está utilizando es Chrome" + navigator.appVersion + navigator.userAgent);
+            
+  }
+  var es_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+  if(es_firefox){
+        alert("El navegador que se está utilizando es Firefox" + navigator.appVersion + navigator.userAgent);
+
+  }
+
+
+  }
