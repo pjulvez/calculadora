6694dfca94af02cd2e98465993d7aa8228e56cac
pjulvez <pjulvez@almata.cat>
2020-11-26 19:04:07 +0100

    canvis en el js per fer el codi més optim de les dos calculadores

--- html/calculadoraNormal.js
+++ html/calculadoraNormal.js
@@ -1,92 +1,92 @@
-window.onload = function(){ //Acciones tras cargar la página
+window.onload = function(){
   pantalla=document.getElementById("textoPantalla");
- //elemento pantalla de salida
-}
-x="0"; //número en pantalla
-xi=1; //iniciar número en pantalla: 1=si; 0=no;
-coma=0; //estado coma decimal 0=no, 1=si;
-ni=0; //número oculto o en espera.
-op="no"; //operación en curso; "no" =  sin operación.
 
-//mostrar número en pantalla según se va escribiendo:
-function numero(xx) { //recoge el número pulsado en el argumento.
-         if (x=="0" || xi==1  ) {	// inicializar un número,
-            pantalla.innerHTML=xx; //mostrar en pantalla
-            x=xx; //guardar número
-            if (xx==".") { //si escribimos una coma al principio del número
-               pantalla.innerHTML="0."; //escribimos 0.
-               x=xx; //guardar número
-               coma=1; //cambiar estado de la coma
+}
+x="0";
+numPantalla=1;
+coma=0;
+numEspera=0;
+operacio="no";
+
+
+function numero(num2) {
+         if (x=="0" || numPantalla==1  ) {
+            pantalla.innerHTML=num2;
+            x=num2;
+            if (num2==".") {
+               pantalla.innerHTML="0.";
+               x=num2; //
+               coma=1;
                }
            }
-           else { //continuar escribiendo un número
-               if (xx=="." && coma==0) { //si escribimos una coma decimal pòr primera vez
-                   pantalla.innerHTML+=xx;
-                   x+=xx;
-                   coma=1; //cambiar el estado de la coma
+           else {
+               if (num2=="." && coma==0) {
+                   pantalla.innerHTML+=num2;
+                   x+=num2;
+                   coma=1;
                }
-               //si intentamos escribir una segunda coma decimal no realiza ninguna acción.
-               else if (xx=="." && coma==1) {}
-               //Resto de casos: escribir un número del 0 al 9:
+
+               else if (num2=="." && coma==1) {}
+
                else {
-                   pantalla.innerHTML+=xx;
-                   x+=xx
+                   pantalla.innerHTML+=num2;
+                   x+=num2
                }
             }
-            xi=0 //el número está iniciado y podemos ampliarlo.
+            numPantalla=0
          }
-function operar(s) {
-           igualar() //si hay operaciones pendientes se realizan primero
-           ni=x //ponemos el 1º número en "numero en espera" para poder escribir el segundo.
-           op=s; //guardamos tipo de operación.
-           xi=1; //inicializar pantalla.
+function operar(tipusOperacio) {
+           igual()
+           numEspera=x
+           operacio=tipusOperacio;
+           numPantalla=1;
          }
-function igualar() {
-         if (op=="no") { //no hay ninguna operación pendiente.
-            pantalla.innerHTML=x;	//mostramos el mismo número
+function igual() {
+         if (operacio=="no") {
+              pantalla.innerHTML=x;
             }
-         else { //con operación pendiente resolvemos
-            sl=ni+op+x; // escribimos la operación en una cadena
-            sol=eval(sl) //convertimos la cadena a código y resolvemos
-            pantalla.innerHTML=sol //mostramos la solución
-            x=sol; //guardamos la solución
-            op="no"; //ya no hay operaciones pendientes
-            xi=1; //se puede reiniciar la pantalla.
-            }
+         else {
+                cadenaSolucio=numEspera+operacio+x;
+                solucio=eval(cadenaSolucio)
+                pantalla.innerHTML=solucio
+                x=solucio;
+                operacio="no";
+                numPantalla=1;
+              }
         }
 
-function porcent() {
-           x=x/100 //dividir por 100 el número
-           pantalla.innerHTML=x; //mostrar en pantalla
-           igualar() //resolver y mostrar operaciones pendientes
-           xi=1 //reiniciar la pantalla
+function percentatge() {
+           x=x/100
+           pantalla.innerHTML=x;
+           igualar()
+           numPantalla=1
          }
-function opuest() {
-           nx=Number(x); //convertir en número
-           nx=-nx; //cambiar de signo
-           x=String(nx); //volver a convertir a cadena
-           pantalla.innerHTML=x; //mostrar en pantalla.
+function oposat() {
+           numero=Number(x);
+           numero=-numero;
+           numero=String(numero);
+           pantalla.innerHTML=x;
          }
 
-function retro(){ //Borrar sólo el último número escrito.
-           cifras=x.length; //hayar número de caracteres en pantalla
-           br=x.substr(cifras-1,cifras) //describir último caracter
-           x=x.substr(0,cifras-1) //quitar el ultimo caracter
-           if (x=="") {x="0";} //si ya no quedan caracteres, pondremos el 0
-           if (br==".") {coma=0;} //Si el caracter quitado es la coma, se permite escribirla de nuevo.
-           pantalla.innerHTML=x; //mostrar resultado en pantalla
+function esborrarUltim(){
+           xifre=x.length;
+           esborrar=x.substr(xifre-1,xifre)
+           x=x.substr(0,xifre-1)
+           if (x=="") {x="0";}
+           if (esborrar==".") {coma=0;}
+           pantalla.innerHTML=x;
          }
-function borradoParcial() {
-          pantalla.innerHTML=0; //Borrado de pantalla;
-          x=0; //Borrado indicador número pantalla.
-          coma=0;	//reiniciamos también la coma
+function esborratParcial() {
+          pantalla.innerHTML=0;
+          x=0;
+          coma=0;
         }
-function borradoTotal() {
-           pantalla.innerHTML=0; //poner pantalla a 0
-           x="0"; //reiniciar número en pantalla
-           coma=0; //reiniciar estado coma decimal
-           ni=0 //indicador de número oculto a 0;
-           op="no" //borrar operación en curso.
+function esborratTotal() {
+           pantalla.innerHTML=0;
+           x="0";
+           coma=0;
+           numEspera=0
+           operacio="no"
        }
 function data(){
          var d = new Date();
